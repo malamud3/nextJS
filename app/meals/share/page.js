3970518c04732d0baed9062332a16dfd76752d23
amir <malamud95@gmail.com>
2025-01-17 19:43:26 +0200

    new file:   app/community/page.js 	new file:   app/meals/page.js 	new file:   app/meals/share/page.js 	modified:   app/page.js 	modified:   package-lock.json

---
+++ app/meals/share/page.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export default function Share() {
+  return (
+    <main>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>Share</h1>
+    </main>
+  );
+}
