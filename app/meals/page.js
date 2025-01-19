186893c3298c993f431ee4bd119a6f5de683ac09
amir <malamud95@gmail.com>
2025-01-19 15:00:10 +0200

    Changes to be committed: 	new file:   app/meals/loading-out.js 	new file:   app/meals/loading.module.css 	modified:   app/meals/page.js

--- app/meals/page.js
+++ app/meals/page.js
@@ -3,8 +3,13 @@ import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
 
-export default async function MealsPage() {
+const Meals = async () => {
   const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+};
+
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -18,7 +23,7 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Meals />
       </main>
     </>
   );
