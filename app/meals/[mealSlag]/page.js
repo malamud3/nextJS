5d9eabc296d50702661f8997e257fda5b9395364
amir <malamud95@gmail.com>
2025-01-19 16:17:54 +0200

    Changes to be committed: 	modified:   app/meals/[mealSlag]/page.js 	new file:   app/meals/[mealSlag]/page.module.css 	new file:   app/not-found.js

--- app/meals/[mealSlag]/page.js
+++ app/meals/[mealSlag]/page.js
@@ -1,12 +1,28 @@
 import Link from 'next/link';
+import classes from './page.module.css';
+import Image from 'next/image';
 
-export default function Meal() {
+export default function MealDetailsPage() {
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>Meal</h1>
-      <p>
-        <Link href="/..">back</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>TITLE</h1>
+          <p className={classes.creator}>
+            by <Link href={`mailto:${'EMAIL'}`}>NAME</Link>
+          </p>
+          <p className={classes.summary}>summary</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: '...' }}
+        />
+      </main>
+    </>
   );
 }
