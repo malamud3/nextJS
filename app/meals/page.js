a6d070843d0af12ef7ae7e5856ac0306d5a9e6a9
amir <malamud95@gmail.com>
2025-01-19 14:23:46 +0200

    Changes to be committed: 	modified:   app/meals/page.js 	new file:   app/meals/page.module.css 	new file:   components/meals/meal-item.js 	new file:   components/meals/meal-item.module.css 	new file:   components/meals/meals-grid.js 	new file:   components/meals/meals-grid.module.css

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,18 +1,20 @@
 import Link from 'next/link';
+import classes from './page.module.css';
 
-export default function Meals() {
+export default function MealsPage() {
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>Meals</h1>
-      <p>
-        <Link href="/..">back</Link>
-      </p>
-      <p>
-        <Link href="meals/share">share</Link>
-      </p>
-      <p>
-        <Link href="meals/meal-1">meal-1</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{' '}
+          <span className={classes.highlight}> by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself.</p>
+        <p className={classes.cta}>
+          <Link href="meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}></main>
+    </>
   );
 }
